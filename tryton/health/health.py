f2337b96d607942791f055c523115bc7be777482
Luis Falcon <lfalcon@gnusolidario.org>
2011-09-28 12:43:47 -0300

    Fixes Spanish menu items for 1.3.3 Fix bug 34416: https://savannah.gnu.org/bugs/?34416
    Updated medicament categories with WHO 17th edition

--- tryton/health/health.py
+++ tryton/health/health.py
@@ -290,6 +290,7 @@ class Medicament(ModelSQL, ModelView):
                         ('C', 'C'),
                         ('D', 'D'),
                         ('X', 'X'),
+                        ('N', 'N'),
 
                         ], 'Pregnancy Category', select="1",
                         help='** FDA Pregancy Categories ***\n' \
@@ -318,7 +319,8 @@ class Medicament(ModelSQL, ModelView):
                         ' fetal abnormalities and/or there is positive evidence of human' \
                         ' fetal risk based on adverse reaction data from investigational' \
                         ' or marketing experience, and the risks involved in use of the' \
-                        ' drug in pregnant women clearly outweigh potential benefits.'
+                        ' drug in pregnant women clearly outweigh potential benefits.\n\n' \
+                        'CATEGORY N : Not yet classified'
                         )
     
     presentation = fields.Text('Presentation', help="Packaging")
