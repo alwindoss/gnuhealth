2be6e33449d510cb0f06b3efb30a0d7112d810f6
Sebastian Marro <smarro@thymbra.com>
2012-07-28 02:36:19 -0300

    Add tests to health_reporting and health_services modules
    Task : https://savannah.gnu.org/task/?12126

---
+++ tryton/health_services/tests/test_health_services.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+##############################################################################
+#
+#    GNU Health: The Free Health and Hospital Information System
+#    Copyright (C) 2008-2012  Sebastian Marro <smarro@gnusolidario.org>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import sys, os
+DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
+    '..', '..', '..', '..', '..', 'trytond')))
+if os.path.isdir(DIR):
+    sys.path.insert(0, os.path.dirname(DIR))
+
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import test_view, test_depends
+
+
+class HealthServicesTestCase(unittest.TestCase):
+    '''
+    Test HealthServices module.
+    '''
+
+    def setUp(self):
+        trytond.tests.test_tryton.install_module('health_services')
+
+    def test0005views(self):
+        '''
+        Test views.
+        '''
+        test_view('health_services')
+
+    def test0006depends(self):
+        '''
+        Test depends.
+        '''
+        test_depends()
+
+def suite():
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        HealthServicesTestCase))
+    return suite
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())
