3172d8ced41195a858c7f45046ad8373e19b4bcd
Sebastian Marro <smarro@thymbra.com>
2012-08-27 01:52:42 -0300

    Add wizard package to services setup.py

--- tryton/health_services/setup.py
+++ tryton/health_services/setup.py
@@ -44,6 +44,7 @@ setup(name='trytond_health_services',
     packages=[
         'trytond.modules.health_services',
         'trytond.modules.health_services.tests',
+        'trytond.modules.health_lab.wizard',
         ],
     package_data={
         'trytond.modules.health_services': info.get('xml', []) \
