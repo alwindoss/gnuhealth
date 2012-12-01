9e679bb9c983d19b9c83f4bc174242abbe6501d7
Luis Falcon <lfalcon@gnusolidario.org>
2012-12-01 20:22:22 -0300

    put comment inside the method

--- tryton/health/health.py
+++ tryton/health/health.py
@@ -432,8 +432,9 @@ class PathologyGroup(ModelSQL, ModelView):
     desc = fields.Char('Short Description', required=True)
     info = fields.Text('Detailed information')
 
-    # Upgrade from GNU Health 1.4.5
+    
     def __register__(cls, module_name):
+        # Upgrade from GNU Health 1.4.5
         super(PathologyGroup, cls).init(module_name)
 
         cursor = Transaction().cursor
