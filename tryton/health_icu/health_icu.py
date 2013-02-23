2829b9802aaa5c35cd079291b8331e0797bdaf62
Luis Falcon <lfalcon@gnusolidario.org>
2013-02-23 18:41:26 -0300

    health_icu : Include Patient ICU period

--- tryton/health_icu/health_icu.py
+++ tryton/health_icu/health_icu.py
@@ -22,7 +22,7 @@ from trytond.model import ModelView, ModelSQL, ModelSingleton, fields
 from datetime import datetime
 from trytond.pool import Pool
 from trytond.transaction import Transaction
-from trytond.pyson import Eval
+from trytond.pyson import Eval, Not, Bool
 
 
 __all__ = ['InpatientRegistration','InpatientIcu']
@@ -40,7 +40,34 @@ class InpatientIcu(ModelSQL, ModelView):
     'Patient ICU Information'
     __name__ = 'gnuhealth.inpatient.icu'
     
+    
+    def icu_duration(self, name):
+
+        now = datetime.now()
+        admission = datetime.strptime(str(icu_admission_date), '%Y-%m-%d %H:%M:%S')
+
+        if discharged_from_icu:
+            discharge = datetime.strptime(icu_discharge_date, '%Y-%m-%d %H:%M:%S')
+            delta = relativedelta(discharge, admission)
+            msg = "Discharged"
+        else:
+            delta = relativedelta(now, admission)
+            msg = ''
+        years_months_days = str(delta.years) + 'y ' \
+                + str(delta.months) + 'm ' \
+                + str(delta.days) + 'd' + msg
+
+        return years_months_days
+
+    
     name = fields.Many2One('gnuhealth.inpatient.registration',
         'Registration Code', required=True)
-    icu_admission_date = fields.DateTime('Admission', help="ICU Admission Date",required=True)
-    icu_discharge_date = fields.DateTime('Discharge', help="ICU Discharge Date",required=True)
+    icu_admission_date = fields.DateTime('ICU Admission', help="ICU Admission Date",required=True)
+    discharged_from_icu = fields.Boolean('Discharged')
+    icu_discharge_date = fields.DateTime('Discharge', states={
+            'invisible': Not(Bool(Eval('discharged_from_icu'))),
+            'required': Bool(Eval('discharged_from_icu')),
+            },
+        depends=['discharged_from_icu'])
+
+    
