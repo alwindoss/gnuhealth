0db1c98642e9446478193fba16b63ba3f89b61c2
Luis Falcon <lfalcon@gnusolidario.org>
2013-05-19 12:00:10 -0300

    health_surgery : Add patient revised cardiac risk index field to the surgery model

--- tryton/health_surgery/health_surgery.py
+++ tryton/health_surgery/health_surgery.py
@@ -168,6 +168,8 @@ class Surgery(ModelSQL, ModelView):
         ('ps5', 'PS 5 : Moribund patients who are not expected to survive without the operation'),
         ('ps6', 'PS 6 : A declared brain-dead patient who organs are being removed for donor purposes'),
         ], 'ASA Preoperative Physical Status', sort=False)
+    preop_rcri = fields.Many2One('gnuhealth.rcri', 'RCRI',
+        help="Patient Revised Cardiac Risk Index")
 
     extra_info = fields.Text('Extra Info')
 
