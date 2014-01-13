f725aca550358db5d2555fc431ecc25ca6146360
Luis Falcon <falcon@gnu.org>
2014-01-13 19:57:22 +0000

    health_nursing : Update the new health professional field

--- tryton/health_nursing/health_nursing.py
+++ tryton/health_nursing/health_nursing.py
@@ -133,7 +133,7 @@ class PatientRounding(ModelSQL, ModelView):
         User = Pool().get('res.user')
         user = User(Transaction().user)
         login_user_id = int(user.id)
-        cursor.execute('SELECT id FROM party_party WHERE is_doctor=True AND \
+        cursor.execute('SELECT id FROM party_party WHERE is_healthprof=True AND \
             internal_user = %s LIMIT 1', (login_user_id,))
         partner_id = cursor.fetchone()
         if partner_id:
@@ -248,7 +248,7 @@ class PatientAmbulatoryCare(ModelSQL, ModelView):
         User = Pool().get('res.user')
         user = User(Transaction().user)
         login_user_id = int(user.id)
-        cursor.execute('SELECT id FROM party_party WHERE is_doctor=True AND \
+        cursor.execute('SELECT id FROM party_party WHERE is_healthprof=True AND \
             internal_user = %s LIMIT 1', (login_user_id,))
         partner_id = cursor.fetchone()
         if partner_id:
