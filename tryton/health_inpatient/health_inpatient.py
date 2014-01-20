3a4859ed505641a0c7ccd32982bd9b1586b660f4
Luis Falcon <falcon@gnu.org>
2014-01-20 19:18:58 +0000

    health_inpatient : update / improve the retrieval of default health professional for medication log

--- tryton/health_inpatient/health_inpatient.py
+++ tryton/health_inpatient/health_inpatient.py
@@ -460,19 +460,8 @@ class InpatientMedicationLog (ModelSQL, ModelView):
 
     @staticmethod
     def default_health_professional():
-        cursor = Transaction().cursor
-        User = Pool().get('res.user')
-        user = User(Transaction().user)
-        login_user_id = int(user.id)
-        cursor.execute('SELECT id FROM party_party WHERE is_healthprof=True AND \
-            internal_user = %s LIMIT 1', (login_user_id,))
-        partner_id = cursor.fetchone()
-        if partner_id:
-            cursor = Transaction().cursor
-            cursor.execute('SELECT id FROM gnuhealth_healthprofessional WHERE \
-                name = %s LIMIT 1', (partner_id[0],))
-            doctor_id = cursor.fetchone()
-            return int(doctor_id[0])
+        pool = Pool()
+        return pool.get('gnuhealth.healthprofessional').get_health_professional()
 
     @staticmethod
     def default_admin_time():
