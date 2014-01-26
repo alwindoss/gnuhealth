b28f4bb71eb91cf443ea2aa6d5a356f9c439180a
Luis Falcon <falcon@gnu.org>
2014-01-26 21:28:04 +0000

    installer : update header

--- tryton/gnuhealth_install.sh
+++ tryton/gnuhealth_install.sh
@@ -6,7 +6,7 @@
 ##############################################################################
 #
 #    GNU Health: The Free Health and Hospital Information System
-#    Copyright (C) 2008-2013  Luis Falcon <falcon@gnu.org>
+#    Copyright (C) 2008-2014  Luis Falcon <falcon@gnu.org>
 #                             Bruno M. Villasanti <bvillasanti@thymbra.com>
 #
 #    This program is free software: you can redistribute it and/or modify
