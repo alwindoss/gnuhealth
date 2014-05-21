3fe13e927d95fd861608446865657b90327f5de7
Luis Falcon <falcon@gnu.org>
2014-05-21 11:23:31 +0100

    crypto_plugin : Initial plugin commit for Tryton / GNU Health client

---
+++ tryton/backend/plugins/crypto/__init__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    GNU Health: The Free Health and Hospital Information System
+#    Copyright (C) 2008-2014 Luis Falcon <lfalcon@gnusolidario.org>
+#    Copyright (C) 2011-2014 GNU Solidario <health@gnusolidario.org>
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import tryton.rpc as rpc
+from subprocess import *
+import gettext
+import gnupg
+
+_ = gettext.gettext
+
+def sign_document(data):
+    """ Retrieve the hash value of the serialized document and 
+        generates a clearsign signature using the user's private key 
+        on the client side via GNU Privacy Guard - GPG -"""
+        
+    gpg = gnupg.GPG()
+    
+    document_model = data['model']
+
+    record_vals = rpc.execute('model', document_model, 'read', data['ids'],
+        ['document_digest'],  rpc.CONTEXT)
+
+    digest = record_vals[0]['document_digest']
+    
+    gpg_signature = gpg.sign(digest,clearsign=True)
+    
+def get_plugins(model):
+    return [
+        (_('Sign Document'), sign_document),
+    ]
