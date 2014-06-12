518dfe679e1aadb60823079c0a9fea60f5354535
Chris Zimmerman <siv@riseup.net>
2014-06-11 21:48:44 -0700

    FHIR: initial server xml handling code

---
+++ tryton/backend/fhir/server/fhir_xml/config.py
@@ -0,0 +1,6 @@
+import re as re_
+
+ExternalEncoding = 'utf-8'
+Tag_pattern_ = re_.compile(r'({.*})?(.*)')
+String_cleanup_pat_ = re_.compile(r"[\n\r\s]+")
+Namespace_extract_pat_ = re_.compile(r'{(.*)}(.*)')
