a0fad6531e6d8007cd6e918936cfb36f1c7f4325
Bruno M. Villasanti <bvillasanti@thymbra.com>
2014-07-05 07:17:56 -0300

    installer : Refactoring to use only functions from main() controller skeleton.

--- tryton/gnuhealth_install.sh
+++ tryton/gnuhealth_install.sh
@@ -24,9 +24,9 @@
 #
 ##############################################################################
 
-#
-# Main functions/variables declaration
-#
+#-----------------------------------------------------------------------------
+# Variables declaration
+#-----------------------------------------------------------------------------
 
 # Colors constants
 NONE="$(tput sgr0)"
@@ -35,7 +35,17 @@ GREEN="$(tput setaf 2)"
 YELLOW="\n$(tput setaf 3)"
 BLUE="\n$(tput setaf 4)"
 
+# Params
 INSTDIR="$PWD"
+GNUHEALTH_INST_DIR="$PWD"
+GNUHEALTH_VERSION=$(cat version)
+TRYTON_VERSION="3.2"
+TRYTON_BASE_URL="http://downloads.tryton.org"
+
+
+#-----------------------------------------------------------------------------
+# Functions
+#-----------------------------------------------------------------------------
 
 message () {
     # $1 : Message
@@ -49,8 +59,8 @@ message () {
 
 check_requirements() {
     # WGET command
-    message "[INFO] CHECKING REQUIREMENTS" ${BLUE}
-    message "Looking for wget...." ${BLUE}
+    message "[INFO] Checking requirements" ${BLUE}
+    echo -n " -> Looking for wget... "
 
     if ! type wget 2>/dev/null ; then
         message "[ERROR] wget command not found. Please install it or check your PATH variable" ${RED}
@@ -58,14 +68,14 @@ check_requirements() {
     fi
 
     # PYTHON version [2.7.x < 3.x]
-    message "Looking for the Python Interpreter command..." ${BLUE}
+    echo -n " -> Looking for the Python Interpreter command... "
 
     if ! type python 2>/dev/null ; then
         message "[ERROR] Python interpreter not found. Please install it or check your PATH variable." ${RED}
         exit 1
     fi
 
-    local PVERSION=`python -V 2>&1 | grep 2.[7-9].[0-9]`
+    local PVERSION=$(python -V 2>&1 | grep 2.[7-9].[0-9])
 
     if test "${PVERSION}" ; then
         message "[INFO] Found ${PVERSION}" ${BLUE}
@@ -75,14 +85,14 @@ check_requirements() {
         exit 1
     fi
 
-    # PIP COMMAND
-    message "-> Looking for PIP command..." ${BLUE}
+    # PIP command
+    echo " -> Looking for PIP command..."
 
     # Alternative pip names on Debian/ArchLinux/RedHat based distros:
     local PIP_NAMES="pip pip2 pip-python"
     PIP_NAME=""
     for NAME in ${PIP_NAMES}; do
-        if [[ `which ${NAME} 2>/dev/null` ]]; then
+        if [[ $(which ${NAME} 2>/dev/null) ]]; then
             PIP_NAME=${NAME}
             break
         fi
@@ -92,22 +102,67 @@ check_requirements() {
         message "[ERROR] PIP command not found. Please install it or check your PATH variable." ${RED}
         exit 1
     fi
-    
-    #Check main operating system
+
+    # Check main operating system
     case "$OSTYPE" in
-        freebsd*) 
+        freebsd*)
             message "[INFO] Running on FreeBSD" ${GREEN}
-            OS="FREEBSD";;
+            OS="FREEBSD"
+            ;;
         linux*)
-            message "Running on GNU/LINUX" ${GREEN}
+            message "[INFO] Running on GNU/LINUX" ${GREEN}
             OS="GNULINUX"
-            #Check for GNU/Linux Distros
-            GNU_LINUX_DISTRO=`lsb_release -i -s`
-            message "GNU / Linux distro : $GNU_LINUX_DISTRO" ${GREEN};;
-        *) message "Running on Other OS: $OSTYPE" ${YELLOW} ;;
+            # Check for GNU/Linux Distros
+            GNU_LINUX_DISTRO=$(lsb_release -i -s)
+            message "[INFO] GNU / Linux distro: $GNU_LINUX_DISTRO" ${GREEN}
+            ;;
+        *)
+            message "[INFO] Running on Other OS: $OSTYPE" ${YELLOW}
+            ;;
     esac
-    
-    
+
+    message "[INFO] OK." ${GREEN}
+}
+
+
+install_directories() {
+    #
+    # Temporary/staging area.
+    #
+    message "[INFO] Creating temporary directory..." ${YELLOW}
+
+    # global variable
+    TMP_DIR="/tmp/gnuhealth_installer"
+
+    if [[ -e ${TMP_DIR} ]]; then
+        message "[ERROR] Directory ${TMP_DIR} exists. You need to delete it." ${RED}
+        exit 1
+    else
+        mkdir ${TMP_DIR} || exit 1
+    fi
+    message "[INFO] OK." ${GREEN}
+
+    #
+    # Create the destination directories.
+    #
+    message "[INFO] Creating destination directories..." ${YELLOW}
+
+    # global variables
+    BASEDIR="$HOME/gnuhealth"
+    TRYTON_BASEDIR="${BASEDIR}/tryton"
+    TRYTOND_DIR="${TRYTON_BASEDIR}/server"
+    MODULES_DIR="${TRYTOND_DIR}/modules"
+    LOG_DIR="${BASEDIR}/logs"
+    ATTACH_DIR="${HOME}/attach"
+    CUSTOM_MODS_DIR="${MODULES_DIR}/custom"
+
+    # Create GNU Health directories
+    if [[ -e ${BASEDIR} ]]; then
+        message "[ERROR] Directory ${BASEDIR} exists. You need to delete it." ${RED}
+        exit 1
+    else
+        mkdir -p ${MODULES_DIR} ${LOG_DIR} ${ATTACH_DIR} ${CUSTOM_MODS_DIR} ||  exit 1
+    fi
     message "[INFO] OK." ${GREEN}
 }
 
@@ -138,8 +193,11 @@ install_python_dependencies() {
     case "$GNU_LINUX_DISTRO" in
         Ubuntu)
             local PIP_PKGS="$PIP_PYTZ $PIP_SIX $PIP_LXML $PIP_RELATORIO $PIP_DATEUTIL $PIP_PSYCOPG2 $PIP_VOBJECT $PIP_PYWEBDAV $PIP_QRCODE $PIP_PILLOW $PIP_CALDAV $PIP_POLIB $PIP_SQL"
-            message "[WARNING] Ubuntu detected : Skipping local PYTHON-LDAP installation. Please refer to the Wikibook to install it" ${YELLOW};;
-        *)  local PIP_PKGS="$PIP_PYTZ $PIP_SIX $PIP_LXML $PIP_RELATORIO $PIP_DATEUTIL $PIP_PSYCOPG2 $PIP_LDAP $PIP_VOBJECT $PIP_PYWEBDAV $PIP_QRCODE $PIP_PILLOW $PIP_CALDAV $PIP_POLIB $PIP_SQL";;
+            message "[WARNING] Ubuntu detected : Skipping local PYTHON-LDAP installation. Please refer to the Wikibook to install it" ${YELLOW}
+            ;;
+        *)
+            local PIP_PKGS="$PIP_PYTZ $PIP_SIX $PIP_LXML $PIP_RELATORIO $PIP_DATEUTIL $PIP_PSYCOPG2 $PIP_LDAP $PIP_VOBJECT $PIP_PYWEBDAV $PIP_QRCODE $PIP_PILLOW $PIP_CALDAV $PIP_POLIB $PIP_SQL"
+            ;;
     esac
 
     message "[INFO] Installing python dependencies with pip-${PIP_VERSION} ..." ${YELLOW}
@@ -152,43 +210,93 @@ install_python_dependencies() {
 }
 
 
-install_directories() {
-    #
-    # Temporary/staging area.
-    #
-    message "[INFO] Creating temporary directory..." ${YELLOW}
+get_url() {
+    # $1 : Module name
+    # return : URL to download
+    echo ${TRYTON_BASE_URL}/${TRYTON_VERSION}/$(wget --quiet -O- ${TRYTON_BASE_URL}/${TRYTON_VERSION} | egrep -o "${1}-${TRYTON_VERSION}.[0-9\.]+.tar.gz" | sort -V | tail -1)
+}
 
-    TMP_DIR="/tmp/gnuhealth_installer"
 
-    if [ -e ${TMP_DIR} ] ; then
-        message "[ERROR] Directory ${TMP_DIR} exists. You need to delete it." ${RED}
-        exit 1
-    else
-        mkdir ${TMP_DIR} || exit 1
-    fi
+install_tryton_modules() {
+    #
+    # Get the lastest revision number for each Tryton module.
+    #
+    message "[INFO] Getting list of lastest Tryton packages..." ${YELLOW}
+
+    local TRYTOND_URL=$(get_url trytond)
+    local TRYTOND_FILE=$(basename ${TRYTOND_URL})
+
+    local TRYTON_MODULES="account account_invoice account_product calendar company country currency party product stock stock_lot"
+
+    local TRYTON_MODULES_FILE=""
+    local TRYTON_MODULES_URL=""
+    local AUX="" MODULE=""
+    for MODULE in ${TRYTON_MODULES}
+    do
+        AUX=$(get_url trytond_${MODULE})
+        TRYTON_MODULES_URL="${TRYTON_MODULES_URL} ${AUX}"
+        TRYTON_MODULES_FILE="${TRYTON_MODULES_FILE} $(basename ${AUX})"
+    done
+
     message "[INFO] OK." ${GREEN}
 
     #
-    # Create the destination directories.
+    # Download Tryton packages.
     #
-    message "[INFO] Creating destination directories..." ${YELLOW}
+    message "[INFO] Changing to temporary directory." ${BLUE}
+    cd ${TMP_DIR} || exit 1
 
-    BASEDIR="$HOME/gnuhealth"
-    TRYTON_BASEDIR="${BASEDIR}/tryton"
-    TRYTOND_DIR="${TRYTON_BASEDIR}/server"
-    MODULES_DIR="${TRYTOND_DIR}/modules"
-    LOG_DIR="${BASEDIR}/logs"
-    ATTACH_DIR="${HOME}/attach"
-    CUSTOM_MODS_DIR="${MODULES_DIR}/custom"
-    
-    # Create GNU Health directories
-    if [ -e ${BASEDIR} ] ; then
-        message "[ERROR] Directory ${BASEDIR} exists. You need to delete it." ${RED}
-        exit 1
-    else
-        mkdir -p ${MODULES_DIR} ${LOG_DIR} ${ATTACH_DIR} ${CUSTOM_MODS_DIR} ||  exit 1
-    fi
+    message "[INFO] Downloading the Tryton server..." ${YELLOW}
+    wget ${TRYTOND_URL} || exit 1
     message "[INFO] OK." ${GREEN}
+
+    message "[INFO] Downloading Tryton modules..." ${YELLOW}
+    local URL=""
+    for URL in ${TRYTON_MODULES_URL}; do
+        wget ${URL} || exit 1
+    done
+    message "[INFO] OK." ${GREEN}
+
+    #
+    # Uncompress the Tryton packages.
+    #
+    message "[INFO] Uncompressing the Tryton server..." ${YELLOW}
+    cd ${TRYTOND_DIR}
+    tar -xzf ${TMP_DIR}/${TRYTOND_FILE} || exit 1
+    message "[INFO] OK." ${GREEN}
+
+    message "[INFO] Uncompressing the Tryton modules..." ${YELLOW}
+    cd ${MODULES_DIR} || exit 1
+    for MODULE in $(ls ${TMP_DIR}/trytond_*); do
+        tar -xzf ${MODULE} || exit 1
+    done
+    message "[INFO] OK." ${GREEN}
+
+    #
+    # Links to modules.
+    #
+    message "[INFO] Changing directory to <../trytond/modules>." ${BLUE}
+    local TRYTOND_FOLDER=$(basename ${TRYTOND_FILE} .tar.gz)
+    cd "${TRYTOND_DIR}/${TRYTOND_FOLDER}/trytond/modules" || exit 1
+
+    message "[INFO] Linking the Tryton modules..." ${YELLOW}
+    local LNMOD=""
+    for LNMOD in ${TRYTON_MODULES}; do
+        ln -si ${MODULES_DIR}/trytond_${LNMOD}-* ${LNMOD} || exit 1
+    done
+    message "[INFO] OK." ${GREEN}
+
+    message "[INFO] Copying GNU Health modules to the Tryton modules directory..." ${YELLOW}
+    cp -a ${GNUHEALTH_INST_DIR}/health* ${MODULES_DIR} || exit 1
+
+    local EXTRA_FILES="COPYING README version"
+    for FILE in ${EXTRA_FILES}; do
+        cp -a ${GNUHEALTH_INST_DIR}/${FILE} ${BASEDIR} || exit 1
+    done
+
+    ln -si ${MODULES_DIR}/health* .
+    message "[INFO] OK." ${GREEN}
+
 }
 
 
@@ -197,10 +305,10 @@ bash_profile () {
     message "[INFO] Creating or Updating the BASH profile for GNU Health" ${BLUE}
 
     cd $INSTDIR
-    
+
     PROFILE="$HOME/.gnuhealthrc"
 
-    if [ -e ${PROFILE} ] ; then
+    if [[ -e ${PROFILE} ]]; then
         # Make a backup copy of the GNU Health BASH profile if it exists
         message "[INFO] GNU Health BASH Profile exists. Making backup to ${PROFILE}.bak ." ${YELLOW}
         cp ${PROFILE} ${PROFILE}.bak || exit 1
@@ -209,151 +317,58 @@ bash_profile () {
     cp gnuhealthrc ${PROFILE} ||  exit 1
 
     # Load .gnuhealthrc from .bash_profile . If .bash_profile does not exist, create it.
-    if [ -e $HOME/.bash_profile ] ; then
+    if [[ -e $HOME/.bash_profile ]]; then
         grep --silent "source ${PROFILE}" $HOME/.bash_profile || echo "[[ -f ${PROFILE} ]] && source ${PROFILE}" >> $HOME/.bash_profile
     else
         echo "[[ -f ${PROFILE} ]] && source ${PROFILE}" >> $HOME/.bash_profile
     fi
 
     # Include .gnuhealthrc in .bashrc for non-login shells
-    if [ -e $HOME/.bashrc ] ; then
+    if [[ -e $HOME/.bashrc ]]; then
         grep --silent "source ${PROFILE}" $HOME/.bashrc || echo "[[ -f ${PROFILE} ]] && source ${PROFILE}" >> $HOME/.bashrc
     else
         echo "[[ -f ${PROFILE} ]] && source ${PROFILE}" >> $HOME/.bashrc
     fi
-   
+
 }
 
-#
-# (0) Start.
-#
-GNUHEALTH_INST_DIR=$PWD
-GNUHEALTH_VERSION=`cat version`
 
-message "[INFO] Starting GNU Health ${GNUHEALTH_VERSION} installation..." ${BLUE}
+cleanup() {
+    message "[INFO] Cleaning Up..." ${YELLOW}
+    rm -rf ${TMP_DIR} || exit 1
 
-#
-# (1) Check requirements.
-#
-check_requirements
-
-#
-# (2) Install directories.
-#
-install_directories
-
-
-#
-# (3) Download settings.
-#
-TRYTON_VERSION="3.2"
-TRYTON_BASE_URL="http://downloads.tryton.org"
-
-get_url() {
-    # $1 : Module name
-    # return : URL to download
-
-    echo ${TRYTON_BASE_URL}/${TRYTON_VERSION}/$(wget --quiet -O- ${TRYTON_BASE_URL}/${TRYTON_VERSION} | egrep -o "${1}-${TRYTON_VERSION}.[0-9\.]+.tar.gz" | sort -V | tail -1)
+    message "[INFO] OK." ${GREEN}
 }
 
-#
-# Get the lastest revision number for each Tryton module.
-#
-message "[INFO] Getting list of lastest Tryton packages..." ${YELLOW}
 
-TRYTOND_URL=$(get_url trytond)
-TRYTOND_FILE=$(basename ${TRYTOND_URL})
+#-----------------------------------------------------------------------------
+# Main
+#-----------------------------------------------------------------------------
 
-TRYTON_MODULES="account account_invoice account_product calendar company country currency party product stock stock_lot"
+main() {
+    message "[INFO] Starting GNU Health ${GNUHEALTH_VERSION} installation..." ${BLUE}
 
-TRYTON_MODULES_FILE=""
-TRYTON_MODULES_URL=""
-for MODULE in ${TRYTON_MODULES}
-do
-    AUX=$(get_url trytond_${MODULE})
-    TRYTON_MODULES_URL="${TRYTON_MODULES_URL} ${AUX}"
-    TRYTON_MODULES_FILE="${TRYTON_MODULES_FILE} $(basename ${AUX})"
-done
+    # (1) Check requirements.
+    check_requirements
 
-message "[INFO] OK." ${GREEN}
+    # (2) Install directories.
+    install_directories
 
+    # (3) Install Python dependencies.
+    install_python_dependencies
 
-#
-# (4) Install Python dependencies.
-#
-install_python_dependencies
+    # (4) Download Tryton modules.
+    install_tryton_modules
 
+    # (5) BASH Profile
+    bash_profile
 
-#
-# (5) Download Tryton packages.
-#
-message "[INFO] Changing to temporary directory." ${BLUE}
-cd ${TMP_DIR} || exit 1
+    # (6) Clean
+    cleanup
 
-message "[INFO] Downloading the Tryton server..." ${YELLOW}
-wget ${TRYTOND_URL} || exit 1
-message "[INFO] OK." ${GREEN}
-
-message "[INFO] Downloading Tryton modules..." ${YELLOW}
-for URL in ${TRYTON_MODULES_URL}; do
-    wget ${URL} || exit 1
-done
-message "[INFO] OK." ${GREEN}
+    message "[INFO] Installed successfully in ${BASEDIR}." ${BLUE}
+}
 
 
-#
-# (6) Uncompress the Tryton packages.
-#
-message "[INFO] Uncompressing the Tryton server..." ${YELLOW}
-cd ${TRYTOND_DIR}
-tar -xzf ${TMP_DIR}/${TRYTOND_FILE} || exit 1
-message "[INFO] OK." ${GREEN}
+main "$@"
 
-message "[INFO] Uncompressing the Tryton modules..." ${YELLOW}
-cd ${MODULES_DIR} || exit 1
-for MODULE in `ls ${TMP_DIR}/trytond_*`; do
-    tar -xzf ${MODULE} || exit 1
-done
-message "[INFO] OK." ${GREEN}
-
-
-#
-# (7) Links to modules.
-#
-message "[INFO] Changing directory to <../trytond/modules>." ${BLUE}
-TRYTOND_FOLDER=$(basename ${TRYTOND_FILE} .tar.gz)
-cd "${TRYTOND_DIR}/${TRYTOND_FOLDER}/trytond/modules" || exit 1
-
-message "[INFO] Linking the Tryton modules..." ${YELLOW}
-for LNMOD in ${TRYTON_MODULES}; do
-    ln -si ${MODULES_DIR}/trytond_${LNMOD}-* ${LNMOD} || exit 1
-done
-message "[INFO] OK." ${GREEN}
-
-message "[INFO] Copying GNU Health modules to the Tryton modules directory..." ${YELLOW}
-cp -a ${GNUHEALTH_INST_DIR}/health* ${MODULES_DIR} || exit 1
-
-EXTRA_FILES="COPYING README version"
-for FILE in ${EXTRA_FILES}; do
-    cp -a ${GNUHEALTH_INST_DIR}/${FILE} ${BASEDIR} || exit 1
-done
-
-ln -si ${MODULES_DIR}/health* .
-message "[INFO] OK." ${GREEN}
-
-
-#
-# (8) BASH Profile
-#
-bash_profile
-
-
-#
-# Clean.
-#
-message "[INFO] Cleaning Up..." ${YELLOW}
-rm -rf ${TMP_DIR} || exit 1
-
-message "[INFO] OK." ${GREEN}
-
-message "[INFO] Installed successfully in ${BASEDIR}." ${BLUE}
