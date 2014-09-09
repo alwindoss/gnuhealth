2b061a6c48de83b705c01540fe247f60e30bb687
Luis Falcon <falcon@gnu.org>
2014-09-09 10:21:55 +0100

    health: Unique constraint in insurance ID/number as per each insurance company

--- tryton/health/health.py
+++ tryton/health/health.py
@@ -1654,7 +1654,7 @@ class Insurance(ModelSQL, ModelView):
     member_since = fields.Date('Member since')
     member_exp = fields.Date('Expiration date')
     category = fields.Char(
-        'Category', help='Insurance company plan / category')
+        'Category', help='Insurance company category')
 
     insurance_type = fields.Selection([
         (None, ''),
@@ -1672,6 +1672,13 @@ class Insurance(ModelSQL, ModelView):
     def get_rec_name(self, name):
         return (self.company.name + ' : ' + self.number)
 
+    @classmethod
+    def __setup__(cls):
+        super(Insurance, cls).__setup__()
+        cls._sql_constraints = [
+            ('number_uniq', 'UNIQUE(number,company)',
+                'The number must be unique per insurance company'),
+        ]
 
 class AlternativePersonID (ModelSQL, ModelView):
     'Alternative person ID'
