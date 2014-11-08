b1f4ce420e7ab05a269a1da7276853a5742c957a
Chris Zimmerman <siv@riseup.net>
2014-11-07 19:47:18 -0800

    FHIR: oops, add blueprint

---
+++ tryton/backend/fhir/server/health_fhir_practitioner_blueprint.py
@@ -0,0 +1,217 @@
+from flask import Blueprint, request, current_app, make_response
+from flask.ext.restful import Resource, abort, reqparse
+from StringIO import StringIO
+from lxml.etree import XMLSyntaxError
+from health_fhir import health_Practitioner, health_OperationOutcome, parse, parseEtree, Bundle, find_record, health_Search
+from extensions import tryton
+from flask.ext.restful import Api
+import lxml
+import json
+import os.path
+import sys
+
+# Practitioner model
+practitioner = tryton.pool.get('gnuhealth.healthprofessional')
+
+# 'Practitioner' blueprint on '/Practitioner'
+practitioner_endpoint = Blueprint('practitioner_endpoint', __name__,
+                                template_folder='templates',
+                                url_prefix="/Practitioner")
+# Initialize api restful
+api = Api(practitioner_endpoint)
+
+class Create(Resource):
+    @tryton.transaction()
+    def post(self):
+        '''Create interaction'''
+        try:
+            c=StringIO(request.data)
+            res=parse(c, silence=True)
+            c.close()
+            res.set_models()
+            p = res.create_patient(subdivision=subdivision,
+                                party=party,
+                                country=country,
+                                contact=contact,
+                                lang=lang,
+                                du=du,
+                                patient=patient)
+        except:
+            e=sys.exc_info()[1]
+            oo=health_OperationOutcome()
+            oo.add_issue(details=e, severity='fatal')
+            return oo, 400
+        else:
+            return 'Created', 201, {'Location': ''.join(['/Patient/', str(p.id)])}
+
+class Search(Resource):
+    @tryton.transaction()
+    def get(self):
+        '''Search interaction'''
+        return 'work in progress', 405
+        s = health_Search(endpoint='practitioner')
+        queries=s.get_queries(request.args)
+        bd=Bundle(request=request)
+        try:
+            for query in queries:
+                if query['query'] is not None:
+                    recs = practitioner.search(query['query'])
+                    if recs:
+                        for rec in recs:
+                            p = health_Practitioner(gnu_record=rec)
+                            bd.add_entry(p)
+            if bd.entries:
+                return bd, 200
+            else:
+                st =[]
+                for k,v in request.args.items():
+                    st.append(':'.join([k,v]))
+                return 'No matching record(s) for {0}'.format(' '.join(st)), 403
+        except:
+            oo=health_OperationOutcome()
+            oo.add_issue(details=sys.exc_info()[1], severity='fatal')
+            return oo, 400
+
+class Validate(Resource):
+    @tryton.transaction()
+    def post(self, log_id=None):
+        '''Validate interaction'''
+        try:
+            # 1) Must correctly parse as XML
+            c=StringIO(request.data)
+            doc=lxml.etree.parse(c)
+            c.close()
+        except XMLSyntaxError as e:
+            oo=health_OperationOutcome()
+            oo.add_issue(details=e, severity='fatal')
+            return oo, 400
+
+        except:
+            e = sys.exc_info()[1]
+            oo=health_OperationOutcome()
+            oo.add_issue(details=e, severity='fatal')
+            return oo, 400
+
+        else:
+            if os.path.isfile('schemas/practitioner.xsd'):
+                # 2) Validate against XMLSchema
+                with open('schemas/practitioner.xsd') as t:
+                    sch=lxml.etree.parse(t)
+
+                xmlschema=lxml.etree.XMLSchema(sch)
+                if not xmlschema.validate(doc):
+                    error = xmlschema.error_log.last_error
+                    oo=health_OperationOutcome()
+                    oo.add_issue(details=error.message, severity='error')
+                    return oo, 400
+            else:
+                # 2) If no schema, check if it correctly parses to a Practitioner
+                try:
+                    pat=parseEtree(StringIO(doc))
+                    if not isinstance(pat, health_Practitioner):
+                        oo=health_OperationOutcome()
+                        oo.add_issue(details='Not a practitioner resource', severity='error')
+                        return oo, 400
+                except:
+                    e = sys.exc_info()[1]
+                    oo=health_OperationOutcome()
+                    oo.add_issue(details=e, severity='fatal')
+                    return oo, 400
+
+            if log_id:
+                # 3) Check if practitioner exists
+                record = find_record(practitioner, [('id', '=', log_id)])
+                if not record:
+                    oo=health_OperationOutcome()
+                    oo.add_issue(details='No practitioner', severity='error')
+                    return oo, 422
+                else:
+                    #TODO: More checks
+                    return 'Valid update', 200
+            else:
+                # 3) Passed checks
+                return 'Valid', 200
+
+class Record(Resource):
+    @tryton.transaction()
+    def get(self, log_id):
+        '''Read interaction'''
+        record = find_record(practitioner, [('id', '=', log_id)])
+        if record:
+            d=health_Practitioner(gnu_record=record)
+            return d, 200
+        else:
+            return 'Record not found', 404
+            #if track deleted records
+            #return 'Record deleted', 410
+
+    @tryton.transaction()
+    def put(self, log_id):
+        '''Update interaction'''
+        return 'Not supported', 405
+
+    @tryton.transaction()
+    def delete(self, log_id):
+        '''Delete interaction'''
+
+        #For now, don't allow (never allow?)
+        return 'Not implemented', 405
+
+class Version(Resource):
+    @tryton.transaction()
+    def get(self, log_id, v_id=None):
+        '''Vread interaction'''
+
+        #No support for this in Health... yet?
+        return 'Not supported', 405
+
+api.add_resource(Create,
+                        '')
+api.add_resource(Search,
+                        '',
+                        '/_search')
+api.add_resource(Validate,
+                        '/_validate',
+                        '/_validate/<int:log_id>')
+api.add_resource(Record, '/<int:log_id>')
+api.add_resource(Version,
+                        '/<int:log_id>/_history',
+                        '/<int:log_id>/_history/<string:v_id>')
+
+@api.representation('xml')
+@api.representation('text/xml')
+@api.representation('application/xml')
+@api.representation('application/xml+fhir')
+def output_xml(data, code, headers=None):
+    if hasattr(data, 'export_to_xml_string'):
+        resp = make_response(data.export_to_xml_string(), code)
+    elif hasattr(data, 'export'):
+        output=StringIO()
+        data.export(outfile=output, namespacedef_='xmlns="http://hl7.org/fhir"', pretty_print=False, level=4)
+        content = output.getvalue()
+        output.close()
+        resp = make_response(content, code)
+    else:
+        resp = make_response(data, code)
+    resp.headers.extend(headers or {})
+    resp.headers['Content-type']='application/xml+fhir' #Return proper type
+    return resp
+
+@api.representation('json')
+@api.representation('application/json')
+@api.representation('application/json+fhir')
+def output_json(data, code, headers=None):
+    resp = make_response(data,code)
+    resp.headers.extend(headers or {})
+    resp.headers['Content-type']='application/json+fhir' #Return proper type
+    return resp
+
+
+@api.representation('atom')
+@api.representation('application/atom')
+@api.representation('application/atom+fhir')
+def output_atom(data, code, headers=None):
+    resp = make_response(data, code)
+    resp.headers.extend(headers or {})
+    resp.headers['Content-type']='application/atom+fhir' #Return proper type
+    return resp
