3dcbf7c997f65de13a89a81d7c757415b3e3e743
Luis Falcon <falcon@gnu.org>
2014-11-09 19:35:04 +0000

    health_reporting : Initial demographics report base

--- tryton/health_reporting/report/summary_report.py
+++ tryton/health_reporting/report/summary_report.py
@@ -19,18 +19,48 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from datetime import datetime
+from datetime import date
 from trytond.report import Report
 from trytond.pool import Pool
 from trytond.transaction import Transaction
+from dateutil.relativedelta import relativedelta
 
 __all__ = ['InstitutionSummaryReport']
 
 class InstitutionSummaryReport(Report):
     __name__ = 'gnuhealth.summary.report'
 
+
     @classmethod
-    def get_new_patients(cls, start_date, end_date):
+    def get_population(cls,date1,date2,sex):
+        """ Return Total Number of people in the system 
+        segmented by age group and sex"""
+        cursor = Transaction().cursor
+
+        cursor.execute("SELECT COUNT(dob) \
+            FROM party_party \
+            WHERE dob BETWEEN %s and %s AND \
+            sex = %s" ,(date2, date1, sex))
+       
+        res = cursor.fetchone()[0]
+    
+        return(res)
+
+    @classmethod
+    def get_new_people(cls, start_date, end_date):
+        """ Return Total Number of new registered persons """
+        cursor = Transaction().cursor
+        cursor.execute("SELECT COUNT(activation_date) \
+            FROM party_party \
+            WHERE activation_date BETWEEN \
+            %s AND %s and is_person=True",(start_date, end_date))
+       
+        res = cursor.fetchone()
+        return(res)
+
+
+    @classmethod
+    def get_new_health_users(cls, start_date, end_date):
         """ Return Total Number of new registered patients """
         cursor = Transaction().cursor
         cursor.execute("SELECT COUNT(activation_date) \
@@ -42,8 +72,9 @@ class InstitutionSummaryReport(Report):
         return(res)
 
     @classmethod
-    def get_total_patients(cls):
-        """ Return Total Number of registered patients in the system """
+    def get_total_health_users(cls):
+        """ Return Total Number of registered people in
+        health care system """
         cursor = Transaction().cursor
         cursor.execute("SELECT COUNT(activation_date) \
             FROM party_party \
@@ -63,11 +94,22 @@ class InstitutionSummaryReport(Report):
         localcontext['start_date'] = data['start_date']
         localcontext['end_date'] = data['end_date']
 
-        localcontext['new_patients'] = \
-            InstitutionSummaryReport().get_new_patients(start_date, end_date)
-        localcontext['total_patients'] = \
-            InstitutionSummaryReport().get_total_patients()
+        # Demographics
+        today = date.today()
+        
+        for age_group in range (0,20):
+            date1 = today - relativedelta(years=(age_group*5))
+            date2 = today - relativedelta(years=((age_group*5)+4))
+            
+            localcontext[''.join(['p',str(age_group),'f'])] = cls.get_population (date1,date2,'f')
+            localcontext[''.join(['p',str(age_group),'m'])] = (-1) * cls.get_population (date1,date2,'m')
+            
+            print age_group*5, date1,'-', age_group*5 + 4, date2
+            
+        #localcontext['population'] = \
+        #    InstitutionSummaryReport().get_population()
 
+        
         return super(InstitutionSummaryReport, cls).parse(report,
             objects, data, localcontext)
 
