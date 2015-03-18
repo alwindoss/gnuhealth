22f5754ffe665e957c33ae70b84f560de2a7721c
Chris Zimmerman <siv@riseup.net>
2015-03-18 15:45:23 -0700

    FHIR: Add route and site valuesets; More work on Immunization resource

--- tryton/backend/fhir/server/health_fhir/health_fhir_immunization.py
+++ tryton/backend/fhir/server/health_fhir/health_fhir_immunization.py
@@ -2,6 +2,7 @@ from StringIO import StringIO
 from operator import attrgetter
 from .datastore import find_record
 import server.fhir as supermod
+from server.common import safe_attrgetter
 
 # URL values
 try:
@@ -20,22 +21,22 @@ class Immunization_Map:
 
     resource_search_params={
             '_id': 'token',
+            'date': 'date',
+            'lot-number': 'string',
+            'dose-sequence': 'number',
+            'performer': 'reference',
+            'vaccine-type': 'token',
             '_language': None,
-            'date': None,
-            'dose-sequence': None,
             'identifier': None,
             'location': None,
-            'lot-number': None,
             'manufacturer': None,
-            'performer': None,
             'reaction': None,
             'reaction-date': None,
             'reason': None,
             'refusal-reason': None,
             'refused': None,
             'requester': None,
-            'subject': None,
-            'vaccine-type': None}
+            'subject': None}
 
     chain_map={ 'subject': 'Patient',
             'performer': 'Practitioner',
@@ -43,17 +44,223 @@ class Immunization_Map:
             'reaction': 'Observation', #TODO AdverseReaction
             'location': 'Location', #TODO
             'manufacturer': 'Organization'} #TODO
+
     search_mapping={
-            '_id': ['id']
-                }
+            '_id': ['id'],
+            'date': ['date'],
+            'subject': ['name'],
+            'lot-number': ['lot.number'],
+            'dose-sequence': ['dose'],
+            'performer': ['healthprof'],
+            'vaccine-type': ['vaccine']
+        }
 
     url_prefixes={}
     model_mapping={'gnuhealth.vaccination':
             {
+                'date': ['date'],
+                'expirationDate': ['lot.expiration_date'],
+                'lot-number': ['lot.number'],
+                'performer': ['healthprof'],
+                'subject': ['name'],
+                'doseQuantity': ['amount'],
+                'site': ['admin-site'],
+                'route': ['admin_route'],
+                'vaccine-type': ['vaccine']
             }}
 
 
 class health_Immunization(supermod.Immunization, Immunization_Map):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        rec = kwargs.pop('gnu_record', None)
+        super(health_Immunization, self).__init__(*args, **kwargs)
+        if rec:
+            self.set_gnu_immunization(rec)
+
+    def set_gnu_immunization(self, procedure):
+        """Set immunization
+        """
+        self.immunization = immunization 
+        self.model_type = self.immunization.__name__
+
+        # Only certain models
+        if self.model_type not in self.model_mapping:
+            raise ValueError('Not a valid model')
+
+        self.map = self.model_mapping[self.model_type]
+
+        self.__import_from_gnu_immunization()
+
+    def __import_from_gnu_immunization(self):
+        """Import the data from the Health model"""
+
+        if self.immunization:
+            self.set_date(safe_attrgetter(self.immunization,
+                    self.map['date']))
+            self.set_expirationDate(safe_attrgetter(self.immunization,
+                    self.map['expirationDate']))
+            self.set_subject(safe_attrgetter(self.immunization,
+                    self.map['subject']))
+            self.set_performer(safe_attrgetter(self.immunization,
+                    self.map['performer']))
+            self.set_lotNumber(safe_attrgetter(self.immunization,
+                    self.map['lotNumber']))
+            self.set_doseQuantity(safe_attrgetter(self.immunization,
+                    self.map['doseQuantity']))
+            self.set_route(safe_attrgetter(self.immunization,
+                    self.map['route']))
+            self.set_site(safe_attrgetter(self.immunization,
+                    self.map['site']))
+            self.set_reported()
+            self.set_refusedIndicator()
+
+    def set_date(self, date):
+        """Extends superclass for convenience
+
+        Keyword arguments:
+        date -- date (datetime object)
+        """
+
+        if date is not None:
+            d = supermod.dateTime(value=date.strftime("%Y/%m/%d"))
+            super(health_Immunization).set_date(d)
+
+    def set_subject(self, subject):
+        """Extends superclass for convenience
+
+        Keyword arguments:
+        subject -- patient (Health model)
+        """
+
+        if subject:
+            if RUN_FLASK:
+                uri = url_for('pat_record', log_id=subject.id)
+            else:
+                uri = dumb_url_generate(['Patient', subject.id])
+            display = patient.rec_name
+            ref=supermod.ResourceReference()
+            ref.display = supermod.string(value=display)
+            ref.reference = supermod.string(value=uri)
+            super(health_Immunization).set_subject(ref)
+
+    def set_performer(self, performer):
+        """Extends superclass for convenience
+
+        Keyword arguments:
+        performer -- practitioner (Health model)
+        """
+
+        if performer:
+            if RUN_FLASK:
+                uri = url_for('hp_record', log_id=performer.id)
+            else:
+                uri = dumb_url_generate(['Practitioner', performer.id])
+            display = performer.rec_name
+            ref=supermod.ResourceReference()
+            ref.display = supermod.string(value=display)
+            ref.reference = supermod.string(value=uri)
+            super(health_Immunization).set_performer(ref)
+
+    def set_lotNumber(self, lotNumber):
+        """Extends superclass for convenience
+
+        Keyword arguments:
+        lotNumber -- lot id (string, number, etc.)
+        """
+
+        if lotNumber is not None:
+            s = supermod.string(value=str(lotNumber))
+            super(health_Immunization).set_lotNumber(s)
+
+    def set_expirationDate(self, expirationDate):
+        """Extends superclass for convenince
+
+        Keyword arguments:
+        expirationDate -- date (datetime object)
+        """
+
+        if expirationDate is not None:
+            d = supermod.date(value=expirationDate.strftime("%Y/%m/%d"))
+            super(health_Immunization).set_expirationDate(d)
+
+    def set_doseQuantity(self, doseQuantity):
+        """Extends superclass for convenience
+
+        Keyword arguments:
+        doseQuantity -- amount in mL
+        """
+
+        if doseQuantity is not None:
+            v = supermod.decimal(value=str(float(doseQuantity)))
+            u = supermod.string(value='mL')
+            s = supermod.uri(value='http://snomed.info/sct')
+            c = supermod.code(value='258773002')
+            dq = supermod.Quantity(value=v, units=u, system=s, code=c)
+            super(health_Immunization).set_doseQuantity(dq)
+
+    def set_refusedIndicator(self, refusedIndicator=False):
+        """Extends superclass for convenience
+
+        Keyword argument:
+        refusedIndicator -- refused? True/False (default: False)
+        """
+
+        b = 'false'
+        if refusedIndicator:
+            b = 'true'
+        ri = supermod.boolean(value=b)
+        super(health_Immunization).set_refusedIndicator(ri)
+
+    def set_reported(self, reported=False):
+        """Extends superclass for convenience
+
+        Keyword argument:
+        reported -- self reported? True/False (default: False)
+        """
+
+        b = 'false'
+        if reported:
+            b = 'true'
+        r = supermod.boolean(value=b)
+        super(health_Immunization).set_reported(r)
+
+    def set_route(self, route):
+        """Extends superclass for convenience
+
+        Keyword argument:
+        route -- how vaccine entered body
+        """
+
+        from server.fhir.valuesets import immunizationRoute
+        if route:
+            ir=[i for i in immunizationRoute.contents if i.code == route.upper()]
+            if ir:
+                cc=supermod.CodeableConcept()
+                cc.text = supermod.string(value=ir[0]['display'])
+                c = supermod.Coding()
+                c.code = supermod.code(value=ir[0]['code'])
+                c.display = supermod.code(value=ir[0]['display'])
+                cc.coding=[c]
+                super(health_Immunization).set_route(cc)
+
+    def set_site(self, site):
+        """Extends superclass for convenience
+
+        Keyword arguments:
+        site -- site code where the vaccine was administered
+        """
+
+        from server.fhir.valuesets import immunizationSite
+        if site:
+            m=[i for i in immunizationSite.contents if i.code == site.upper()]
+            if m:
+                cc=supermod.CodeableConcept()
+                cc.text = supermod.string(value=m[0]['display'])
+                c = supermod.Coding()
+                c.code = supermod.code(value=m[0]['code'])
+                c.display = supermod.code(value=m[0]['display'])
+                cc.coding=[c]
+                super(health_Immunization).set_site(cc)
 
 supermod.Immunization.subclass=health_Immunization
