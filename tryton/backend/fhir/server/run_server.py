4173892e42c51aa33adc722fc3e6c42635289fb4
Chris Zimmerman <siv@riseup.net>
2015-05-08 17:54:46 -0700

    FHIR: Default to the debug config

--- tryton/backend/fhir/server/run_server.py
+++ tryton/backend/fhir/server/run_server.py
@@ -8,7 +8,8 @@ from server import create_app
 from server.config import ProductionConfig
 
 define("port", default=5000, help="Port to listen on", type=int)
-app = create_app(config=ProductionConfig)
+#app = create_app(config=ProductionConfig)
+app = create_app() #DebugConfig
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(options.port)
 IOLoop.instance().start()
