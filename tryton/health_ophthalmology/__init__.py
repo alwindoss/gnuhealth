044ded2b3a35fda912bbf8c6f73b41d0e53109b2
Luis Falcon <falcon@gnu.org>
2015-12-03 18:47:58 +0000

    health_ophthalmology : Consolidate eye structures findings on a Selection field and associate them in a O2M sield on the ophthalmological evaluation

--- tryton/health_ophthalmology/__init__.py
+++ tryton/health_ophthalmology/__init__.py
@@ -3,37 +3,6 @@ from .health_ophthalmology import *
 
 def register():
     Pool.register(
-        OphthalmologyLidFindingsList,
-        OphthalmologyNCSFindingsList,
-        OphthalmologyConjunctivaFindingsList,
-        OphthalmologyCorneaFindingsList,
-        OphthalmologyIrisFindingsList,
-        OphthalmologyAnteriorChamberFindingsList,
-        OphthalmologyPupilFindingsList,
-        OphthalmologyLensFindingsList,
-        OphthalmologyVitreousFindingsList,
-        OphthalmologyFundusDiscFindingsList,
-        OphthalmologyFundusMaculaFindingsList,
-        OphthalmologyFundusBackgroundFindingsList,
-        OphthalmologyFundusVesslesFindingsList,
-        OphthalmologyMiscFindingsList,
-        
-        OphthalmologyFindings,
-    
-        OphthalmologyLidFindings,
-        OphthalmologyNCSFindings,
-        OphthalmologyConjunctivaFindings,
-        OphthalmologyCorneaFindings,
-        OphthalmologyIrisFindings,
-        OphthalmologyAnteriorChamberFindings,
-        OphthalmologyPupilFindings,
-        OphthalmologyLensFindings,
-        OphthalmologyVitreousFindings,
-        OphthalmologyFundusDiscFindings,
-        OphthalmologyFundusMaculaFindings,
-        OphthalmologyFundusBackgroundFindings,
-        OphthalmologyFundusVesslesFindings,
-        OphthalmologyMiscFindings,
-    
-        Ophthalmology,
+        OphthalmologyEvaluation,
+        OphthalmologyFindings,    
         module='health_ophthalmology', type_='model')
