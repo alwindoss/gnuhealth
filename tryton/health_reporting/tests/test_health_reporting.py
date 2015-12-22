55c11ea81921af8d975387f25ffb1550c5a683e8
C?dric Krier <ced@b2ck.com>
2015-12-22 15:28:09 +0100

    health_reporting: Fix unittest and use ModuleTestCase

--- tryton/health_reporting/tests/test_health_reporting.py
+++ tryton/health_reporting/tests/test_health_reporting.py
@@ -1,59 +1,17 @@
-#!/usr/bin/env python
-##############################################################################
-#
-#    GNU Health: The Free Health and Hospital Information System
-#    Copyright (C) 2011-2014 Sebastian Marro <smarro@gnusolidario.org>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-import sys, os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class HealthReportingTestCase(unittest.TestCase):
+class HealthReportingTestCase(ModuleTestCase):
     '''
-    Test HealthReporting module.
+    Test Health Reporting module.
     '''
+    module = 'health_reporting'
 
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('health_reporting')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('health_reporting')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         HealthReportingTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
