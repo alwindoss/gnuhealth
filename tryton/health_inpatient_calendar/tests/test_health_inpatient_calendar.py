072e96dec74376fd8fc688d8cfa5e9e92a60f4b3
C?dric Krier <ced@b2ck.com>
2015-12-23 14:51:02 +0100

    health_inpatient_calendar: Fix unittest and use ModuleTestCase

--- tryton/health_inpatient_calendar/tests/test_health_inpatient_calendar.py
+++ tryton/health_inpatient_calendar/tests/test_health_inpatient_calendar.py
@@ -1,41 +1,17 @@
-#!/usr/bin/env python
-
-import sys, os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class HealthInpatientCalendarTestCase(unittest.TestCase):
+class HealthInpatientCalendarTestCase(ModuleTestCase):
     '''
-    Test HealthInpatientCalendar module.
+    Test Health Inpatient Calendar module.
     '''
+    module = 'health_inpatient_calendar'
 
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('health_inpatient_calendar')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('health_inpatient_calendar')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         HealthInpatientCalendarTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
