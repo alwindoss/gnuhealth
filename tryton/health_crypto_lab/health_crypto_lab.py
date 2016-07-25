260eb49c2a988c87ed16b4304dec8bf6d438ca82
Luis Falcon <falcon@gnu.org>
2016-07-25 12:53:16 +0000

    health_crypto_lab : Initial commit for module . task #14066: Include crypto functionality for laboratory

---
+++ tryton/health_crypto_lab/health_crypto_lab.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    GNU Health: The Free Health and Hospital Information System
+#    Copyright (C) 2008-2016 Luis Falcon <lfalcon@gnusolidario.org>
+#    Copyright (C) 2011-2016 GNU Solidario <health@gnusolidario.org>
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from trytond.model import ModelView, ModelSQL, fields
+from trytond.transaction import Transaction
+from trytond.rpc import RPC
+from trytond.pool import Pool
+from trytond.wizard import Wizard, StateAction, StateView, Button
+from trytond.pyson import Eval, Not, Bool, PYSONEncoder, Equal, And, Or
+import hashlib
+import json
+
+__all__ = ['LabTest']
+
+
+class LabTest(ModelSQL, ModelView):
+    'Lab Test'
+    __name__ = 'gnuhealth.lab'
+
+    serializer = fields.Text('Doc String', readonly=True)
+
+    document_digest = fields.Char('Digest', readonly=True,
+        help="Original Document Digest")
+
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('done', 'Done'),        
+        ('validated', 'Validated'),        
+        ], 'State', readonly=True, sort=False)
+
+
+    digest_status = fields.Function(fields.Boolean('Altered',
+        states={
+        'invisible': Not(Equal(Eval('state'),'done')),
+        },
+        help="This field will be set whenever parts of" \
+        " the main original document has been changed." \
+        " Please note that the verification is done only on selected" \
+        " fields." ),
+        'check_digest')
+
+    serializer_current = fields.Function(fields.Text('Current Doc',
+            states={
+            'invisible': Not(Bool(Eval('digest_status'))),
+            }),
+        'check_digest')
+
+        
+    digest_current = fields.Function(fields.Char('Current Hash',
+            states={
+            'invisible': Not(Bool(Eval('digest_status'))),
+            }),
+        'check_digest')
+
+    digital_signature = fields.Text('Digital Signature', readonly=True)
+
+        
+    @staticmethod
+    def default_state():
+        return 'draft'
+
+    @classmethod
+    def __setup__(cls):
+        cls._buttons.update({
+            'generate_document': {
+                'invisible': Not(Equal(Eval('state'), 'draft')),
+                },
+            'sign_document': {
+                'invisible': Not(Equal(Eval('state'), 'done')),
+                },
+            })
+        ''' Allow calling the set_signature method via RPC '''
+        cls.__rpc__.update({
+                'set_signature': RPC(readonly=False),
+                })
+
+    @classmethod
+    @ModelView.button
+    def generate_document(cls, documents):
+        document = documents[0]
+
+        # Change the state of the evaluation to "Done"
+        # and write the name of the signing health professional
+
+        serial_doc=cls.get_serial(document)
+        
+
+        cls.write(documents, {
+            'serializer': serial_doc,
+            'document_digest': HealthCrypto().gen_hash(serial_doc),
+            'state': 'validated',})
+
+
+    @classmethod
+    def get_serial(cls,document):
+
+        analyte_line=[]
+        
+        for line in document.critearea:
+            line_elements=[line.name or '',
+                line.result or '', 
+                line.result_text or '',
+                line.remarks or '']
+                
+            analyte_line.append(line_elements)
+
+        data_to_serialize = { 
+            'Lab_test': unicode(document.name) or '',
+            'Test': unicode(document.test.rec_name) or '',
+            'HP': unicode(document.requestor.rec_name),
+            'Patient': document.patient.rec_name,
+            'Patient_ID': unicode(document.patient.name.ref) or '',
+            'Analyte_line': analyte_line,
+             }
+
+        serialized_doc = HealthCrypto().serialize(data_to_serialize)
+        
+        return serialized_doc
+    
+    @classmethod
+    def set_signature(cls, data, signature):
+        """
+        Set the clearsigned signature
+        """
+
+        doc_id = data['id']
+        
+        cls.write([cls(doc_id)], {
+            'digital_signature': signature,
+            })
+
+
+
+    def check_digest (self,name):
+        result=''
+        serial_doc=self.get_serial(self)
+        if (name == 'digest_status' and self.document_digest):
+            if (HealthCrypto().gen_hash(serial_doc) == self.document_digest):
+                result = False
+            else:
+                ''' Return true if the document has been altered'''
+                result = True
+        if (name=='digest_current'):
+            result = HealthCrypto().gen_hash(serial_doc)
+
+        if (name=='serializer_current'):
+            result = serial_doc
+            
+        return result
+ 
+    # Hide the group holding validation information when state is 
+    # not validated
+    
+    @classmethod
+    def view_attributes(cls):
+        return [('//group[@id="document_digest"]', 'states', {
+                'invisible': Not(Eval('state') == 'validated'),
+                })]
+       
+
+
+
+class HealthCrypto:
+    """ GNU Health Cryptographic functions
+    """
+
+    def serialize(self,data_to_serialize):
+        """ Format to JSON """
+        json_output = \
+            json.dumps(data_to_serialize, ensure_ascii=False).encode('utf-8')
+        return json_output
+
+    def gen_hash(self, serialized_doc):
+        return hashlib.sha512(serialized_doc).hexdigest()
+
+
