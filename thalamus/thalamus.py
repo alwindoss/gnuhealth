c5e72628f3cf4989aacbb1889882b06f147220ac
Luis Falcon <falcon@gnu.org>
2017-04-11 20:42:41 +0100

    Thalamus : Integrate authentication into thalamus module

--- thalamus/thalamus.py
+++ thalamus/thalamus.py
@@ -19,11 +19,11 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
-
-import security
+from flask_httpauth import HTTPBasicAuth
+import bcrypt
 
 import logging
 
@@ -36,13 +36,37 @@ api = Api(app)
 
 mongo = PyMongo(app)
 
+auth = HTTPBasicAuth()
+
+# Authentication
+
+@auth.verify_password
+def verify_password(username, password):
+    """
+    Takes the username and password from the client
+    and checks them against the entry on the people db collection
+    The password is bcrypt hashed
+    """ 
+    user = mongo.db.people.find_one({'_id' : username})
+    if (user):
+        account = mongo.db.people.find_one({'_id' : username})
+        person = account['_id']
+        hashed_password = account['password']
+        if bcrypt.checkpw(password.encode('utf-8'), 
+            hashed_password.encode('utf-8')):
+            return True
+        else:
+            return False
+        
+    else:
+       return False
 
 
 # People Resource
 class People(Resource):
     "Holds the person demographics information"
     
-    decorators = [security.auth.login_required] # Use the decorator from httpauth
+    decorators = [auth.login_required] # Use the decorator from httpauth
     def get(self):
         """
         Retrieves all the people on the person collection
@@ -55,7 +79,7 @@ class People(Resource):
 class Person(Resource):
     "Holds the person demographics information"
     
-    decorators = [security.auth.login_required] # Use the decorator from httpauth
+    decorators = [auth.login_required] # Use the decorator from httpauth
     def get(self, person_id):
         """
         Retrieves the person instance
