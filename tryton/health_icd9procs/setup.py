9c51707ba298d4d5439e84678084bce2c0e474c3
Luis Falcon <falcon@gnu.org>
2017-06-12 13:54:46 +0100

    Python packaging . Explicit unicode declaration in setup open method

--- tryton/health_icd9procs/setup.py
+++ tryton/health_icd9procs/setup.py
@@ -22,7 +22,7 @@ import os
 import configparser
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding="UTF-8").read()
 
 config = configparser.ConfigParser()
 config.readfp(open('tryton.cfg'))
