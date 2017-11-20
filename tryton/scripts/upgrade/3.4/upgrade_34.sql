a49270199f809c3300a87266c7eb0f1435d22468
Luis Falcon <falcon@gnu.org>
2017-11-20 19:58:00 +0000

    task #14735: Upgrade to Tryton 4.6 Kernel. Include SQL upgrade script

---
+++ tryton/scripts/upgrade/3.4/upgrade_34.sql
@@ -0,0 +1,24 @@
+/*
+# GNU HEALTH : The Free Health and Hospital Information System
+# (c) 2008-2017 Luis Falcon <falcon@gnu.org>
+# (c) 2008-2017 GNU Solidario <info@gnusolidario.org>
+
+# Program name : upgrade_34.sql
+
+# Script to update from GNU Health 3.2 to 3.4 (tryton 4.6)
+
+# Versions affected : Any UPGRADE to 3.4 series from previous releases 
+
+# INSTRUCTIONS :
+#
+# 1) Run the script via psql your_db_name < upgrade_34.sql
+#
+# Execute this script ONLY once !
+
+
+
+*/
+
+UPDATE party_address_format SET format_ = REPLACE(format_, '${district}', '${subdivision}');
+
+DELETE FROM ir_model_data WHERE model = 'ir.property';
