7d8c3f8995265951b2acd9d9a2d0b5f79db62b82
Luis Falcon <falcon@gnu.org>
2018-12-28 13:12:46 +0000

    Update to version 3.4.2

--- gnuhealth-client/tryton/__init__.py
+++ gnuhealth-client/tryton/__init__.py
@@ -1,4 +1,4 @@
 # This file is part of the GNU Health GTK Client.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-__version__ = "3.4.1"
+__version__ = "3.4.2"
 SERVER_VERSION = "4.6.0"
