e65089e57a6ad43baca26405cfa84f2726d84971
Luis Falcon <falcon@gnu.org>
2019-06-24 15:50:27 +0100

    gnuhealth portal : Updated / improved css / styles . Thank you Leo !

--- federation/gnuhealth_portal/src/App.vue
+++ federation/gnuhealth_portal/src/App.vue
@@ -16,10 +16,12 @@
                     class="ghbuttondark">Login</router-link>
             </div>
         </span>
-
         <!-- Show the entry page ("Home") -->
-        <router-view/>
         <br/>
+    </div>
+    <router-view/>
+    <div class="footer">
+    <p><small>GNU Health (c) GNU Solidario GPL v3+ </small></p>
     </div>
   </div>
 </template>
@@ -47,22 +49,54 @@
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #51717e;
+    color: #007191;
+    padding: 30px;
+    padding-top:0px;
     }
     #nav {
-    padding: 30px;
+    padding-top:30px;
+    padding-bottom:30px;
+    }
+
+    div#app div#nav {
+    background-image: url(assets/logo-gnu-health-federation.png);
+    background-repeat: no-repeat;
+    background-position: 0px center;
+    }
+
+    div#nav {
+    border-bottom: 1px solid #bedfe4;
+
+    }
+    div#app div#nav span {
+    float:right;
+    }
+
+    div#app div#nav span a {
+    text-transform: uppercase;
+    font-size: 14px;
+    text-decoration: none;
+    }
+
+    div#app div#nav span a:last-of-type {
+    margin-right: 50px;
     }
 
     #nav a {
     font-weight: bold;
-    color: #51717e;
+    color: #007191;
     }
 
     #nav a.router-link-exact-active {
-    color: #08a8b3;
+    color: #00acc6;
     }
     .required {
         background: #51717e;
+    }
+
+    .footer {
+    clear:both;
+    border-top: 1px solid #bedfe4;
     }
 
 /* Buttons */
@@ -70,20 +104,34 @@
 
 .ghbutton {
     border: none;
-    background: #00bec6;
-    padding: 5px 15px;
+    background: #007191;
+    padding: 8px 25px;
     color: white;
-    border-radius: 20px;
+    border-radius: 4px;
     margin-right: 10px;
+    font-size: 14;
 }
+
+
+.greybutton {
+    background: #95b5bc;
+}
+
+
 
 .ghbuttondark {
     border: none;
-    background: #276777;
     padding: 5px 15px;
     color: white;
     border-radius: 20px;
     margin-right: 10px;
+    background: #00acc6;
+    text-transform: uppercase;
+    padding-left: 25px;
+    padding-right: 25px;
+    font-size: 14px;
+    font-weight: bold;
+    cursor: pointer;
 }
 
 .upper-right {
@@ -93,9 +141,9 @@
 /* Forms */
 
 .gh-form {
-    margin:10px auto;
-    padding: 20px 12px 10px 20px;
-    font: 13px;
+    margin: 0px;
+    padding: 0px;
+    font-size: 14px;
     text-align: left;
 }
 .gh-form li {
@@ -124,7 +172,7 @@ select{
     box-sizing: border-box;
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
-    border:1px solid #BEBEBE;
+    border:1px solid #cfdee0;
     padding: 7px;
     margin:2px;
     -webkit-transition: all 0.30s ease-in-out;
@@ -132,6 +180,9 @@ select{
     -ms-transition: all 0.30s ease-in-out;
     -o-transition: all 0.30s ease-in-out;
     outline: none;
+    border-radius: 4px;
+    color:#007191;
+    margin-right: 12px;
 }
 .gh-form input[type=text]:focus,
 .gh-form input[type=date]:focus,
@@ -144,10 +195,10 @@ select{
 .gh-form input[type=email]:focus,
 .gh-form textarea:focus,
 .gh-form select:focus{
-    -moz-box-shadow: 0 0 8px #00bec6;
-    -webkit-box-shadow: 0 0 8px #00bec6;
-    box-shadow: 0 0 8px #00bec6;
-    border: 1px solid #00bec6;
+    -moz-box-shadow: 0 0 8px #00acc6;
+    -webkit-box-shadow: 0 0 8px #00acc6;
+    box-shadow: 0 0 8px #00acc6;
+    border: 1px solid #00acc6;
 }
 .gh-form .field-divided{
     width: 49%;
@@ -165,4 +216,32 @@ select{
 .gh-form .red{
     color:red;
 }
+
+table.restable {
+    border-bottom: 1px solid #bedfe4;
+    border-top: 1px solid #bedfe4;
+    margin-top: 25px;
+    width: 100%;
+    border-collapse: collapse;
+}
+
+table.restable th {
+    border-bottom: 1px solid #bedfe4;
+    text-transform: uppercase;
+    font-size: 12px;
+    padding-top:10px;
+    padding-bottom: 10px;
+}
+
+table.restable td {
+    padding: 6px;
+}
+
+table.restable tr:nth-child(even) {
+    background: #ffffff;
+    }
+table.restable tr:nth-child(odd) {
+    background: #ecf2f3;
+    }
+
 </style>
