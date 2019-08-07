5b848b50f7c967a6f9bb10dedd245069905c39c8
Luis Falcon <falcon@gnu.org>
2019-08-07 11:14:14 +0100

    gnuhealth plugins: Update version numbers to development 3.5

--- plugins/crypto/__init__.py
+++ plugins/crypto/__init__.py
@@ -2,8 +2,8 @@
 ##############################################################################
 #
 #    GNU Health: The Free Health and Hospital Information System
-#    Copyright (C) 2008-2018 Luis Falcon <lfalcon@gnusolidario.org>
-#    Copyright (C) 2011-2018 GNU Solidario <health@gnusolidario.org>
+#    Copyright (C) 2008-2019 Luis Falcon <lfalcon@gnusolidario.org>
+#    Copyright (C) 2011-2019 GNU Solidario <health@gnusolidario.org>
 #
 #
 #    This program is free software: you can redistribute it and/or modify
