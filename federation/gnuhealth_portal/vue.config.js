6abb3bda5b6002b3404d6b87ea04b59bd8c3c5a8
Luis Falcon <falcon@gnu.org>
2019-09-05 23:14:03 +0100

    gnuhealth federation portal: Use SSL by default at the GNU Health portal

--- federation/gnuhealth_portal/vue.config.js
+++ federation/gnuhealth_portal/vue.config.js
@@ -1,8 +1,12 @@
+const fs = require('fs');
+
 module.exports = {
   devServer: {
     host: '0.0.0.0',
-    port: 8085, 
-    https: false,
+    port: 8444, 
+    https: true,
+    key: fs.readFileSync('/opt/gnuhealth/certs/gnuhealthfed.key'),
+    cert: fs.readFileSync('/opt/gnuhealth/certs/gnuhealthfed.crt'),
     hotOnly: false,
   },
 }
