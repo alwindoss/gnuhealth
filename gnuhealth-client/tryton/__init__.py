74754cbcddedd4f57c50979e1393c9d3febe4e11
Luis Falcon <falcon@gnuhealth.org>
2019-12-30 17:24:52 +0000

    GNU Health GTK client : Update stable branch to 3.6.5

--- gnuhealth-client/tryton/__init__.py
+++ gnuhealth-client/tryton/__init__.py
@@ -1,6 +1,6 @@
 # This file is part of GNU Health.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-__version__ = "3.6.3"
+__version__ = "3.6.5"
 SERVER_VERSION = "5.0.0"
 import gi
 
