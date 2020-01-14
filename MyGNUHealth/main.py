a0cdd62c41fd17d379557cb14f02e676ba2d425a
Luis Falcon <falcon@gnuhealth.org>
2020-01-14 11:35:28 +0000

    MyGNUHealth : PoC authentication to Thalamus

--- MyGNUHealth/main.py
+++ MyGNUHealth/main.py
@@ -31,10 +31,48 @@ from PySide2.QtWidgets import QApplication
 from PySide2.QtCore import QObject, QUrl, Slot
 from PySide2.QtQml import QQmlApplicationEngine
 
+import requests
+
 class FedLogin(QObject):
     @Slot(str, str)
     def login(self, acct, passwd):
-        print(acct, passwd)
+        self.test_connection(acct, passwd)
+
+    def test_connection(self,acct, passwd):
+        """ Make the connection test to Thalamus Server
+            from the GNU Health HMIS using the institution
+            associated admin and the related credentials
+        """
+        conn = ''
+        host, port, user, password, ssl_conn, verify_ssl = \
+            'localhost', 8443,  \
+            acct, passwd, True, \
+            False
+
+        if (ssl_conn):
+            protocol = 'https://'
+        else:
+            protocol = 'http://'
+
+        if (not user or not password):
+            print("Please provide login credentials")
+
+
+        url = protocol + host + ':' + str(port) + '/people/' + user
+
+        try:
+            conn = requests.get(url,
+                auth=(user, password), verify=verify_ssl)
+
+        except:
+            print("ERROR authenticating to Server")
+
+        if conn:
+            print("***** Connection to Thalamus Server OK !******")
+
+        else:
+            print("##### Wrong credentials ####")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
