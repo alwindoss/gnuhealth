42e2770cd76be7353b46537ef9f8359d7d8d3c06
Luis Falcon <falcon@gnuhealth.org>
2020-05-16 12:48:01 +0100

    HMIS: Include matplotlib dependency

--- tryton/health/setup.py
+++ tryton/health/setup.py
@@ -34,7 +34,7 @@ for key in ('depends', 'extras_depend', 'xml'):
         info[key] = info[key].strip().splitlines()
 major_version, minor_version = 5, 0
 
-requires = ['pytz','numpy']
+requires = ['pytz','numpy','matplotlib']
 
 for dep in info.get('depends', []):
     if (dep == 'health'):
