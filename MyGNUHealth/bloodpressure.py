1eea1123d697997d18edb0f22e398aafb06d6699
Luis Falcon <falcon@gnuhealth.org>
2020-05-21 18:05:33 +0100

    store in local DB the values from blood pressure, heart rate and date in isoformat

--- MyGNUHealth/bloodpressure.py
+++ MyGNUHealth/bloodpressure.py
@@ -1,6 +1,7 @@
 from PySide2.QtCore import QObject, Signal, Slot, Property
 from tinydb import TinyDB, Query
 from myghconf import dbfile
+import datetime
 
 class BloodPressure(QObject):
     def __init__(self):
@@ -8,9 +9,25 @@ class BloodPressure(QObject):
 
     db = TinyDB(dbfile)
 
+    def insert_values(self, systolic, diastolic, heart_rate):
+        blood_pressure = self.db.table('bloodpressure')
+        hr = self.db.table('heart_rate')
+        current_date = datetime.datetime.now().isoformat()
+
+        if ((systolic > 0) and (diastolic > 0)):
+            blood_pressure.insert({'timestamp': current_date,
+                                   'systolic': systolic,
+                                   'diastolic': diastolic})
+            print ("Saved blood pressure",systolic, diastolic)
+
+        if (heart_rate > 0):
+            hr.insert ({'timestamp':current_date, 'heart_rate':heart_rate})
+            print ("Saved Heart rate", heart_rate)
+
+
     @Slot (int, int, int)
-    def getvals(self,systolic, diastolic, rate):
-        print (systolic, diastolic, rate)
+    def getvals(self,systolic, diastolic, heart_rate):
+        self.insert_values(systolic, diastolic, heart_rate)
         self.setOK.emit()
 
     # Signal to emit to QML if the blood pressure values were stored correctly
