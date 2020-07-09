899c79d0674c4dd10db5646b3652ce7544e6f748
Luis Falcon <falcon@gnuhealth.org>
2020-07-09 14:11:48 +0100

    MyGNUHealth : Improve BP / HR methods and properties * Use QVariantList in bp property * Improve QML properties

--- MyGNUHealth/bloodpressure.py
+++ MyGNUHealth/bloodpressure.py
@@ -18,11 +18,11 @@ class BloodPressure(QObject):
             blood_pressure.insert({'timestamp': current_date,
                                    'systolic': systolic,
                                    'diastolic': diastolic})
-            print ("Saved blood pressure",systolic, diastolic)
+            print ("Saved blood pressure",systolic, diastolic, current_date)
 
         if (heart_rate > 0):
             hr.insert ({'timestamp':current_date, 'heart_rate':heart_rate})
-            print ("Saved Heart rate", heart_rate)
+            print ("Saved Heart rate", heart_rate, current_date)
 
 
     @Slot (int, int, int)
