c8d6dff27205c75f4a18e01a2fb3309701a765ad
Luis Falcon <falcon@gnuhealth.org>
2020-07-12 16:55:46 +0100

    MYGNUHealth: Initial charts for Blood pressure and Heart Rate history

--- MyGNUHealth/bio.py
+++ MyGNUHealth/bio.py
@@ -12,20 +12,51 @@ class GHBio(QObject):
     db = TinyDB(dbfile)
 
     def read_bp(self):
+        #Retrieve all the BP / HR history
         blood_pressure = self.db.table('bloodpressure')
         hr = self.db.table('heart_rate')
-        latestbp = blood_pressure.all()[-1]  # Get the latest (newest) record
-        latesthr = hr.all()[-1]
-        return (latestbp,latesthr)
+        bphist = blood_pressure.all()
+        hrhist = hr.all()
+        return (bphist,hrhist)
 
     def getBP(self):
-        bp,hr = self.read_bp()
+        # Extracts the latest readings from BP / HR
+        bphist,hrhist = self.read_bp()
+        bp = bphist[-1]  # Get the latest (newest) record
+        hr = hrhist[-1]
+
         dateobj =  datetime.datetime.fromisoformat(bp['timestamp'])
         date_repr = dateobj.strftime("%a, %b %d '%y - %H:%M")
 
         bpobj = [str(date_repr), str(bp['systolic']), str(bp['diastolic']),
                                      str(hr['heart_rate'])]
         return bpobj
+
+    def getBPHist(self):
+        # Retrieves all the history and packages into an array.
+        bphist,hrhist = self.read_bp()
+        bphrhist = []
+
+        """TODO:
+            The best would be to be able to pass the JSON dictionary
+            instead of an array of arrays to QVariantList.
+
+        """
+
+        bpsys = []
+        bpdia = []
+        hr = []
+        for element in bphist:
+            bpsys.append(element['systolic'])
+            bpdia.append(element['diastolic'])
+        bphrhist.append(bpsys)
+        bphrhist.append(bpdia)
+
+        for element in hrhist:
+            hr.append(element['heart_rate'])
+        bphrhist.append(hr)
+
+        return bphrhist
 
     def setBP(self, bp):
         self.current_bp = bp
@@ -37,3 +68,6 @@ class GHBio(QObject):
 
     # BP property to be accessed to and from QML and Python.
     bp = Property("QVariantList", getBP, setBP, notify=bpChanged)
+
+    # BP property to be accessed to and from QML and Python.
+    bphist = Property("QVariantList", getBPHist, setBP, notify=bpChanged)
