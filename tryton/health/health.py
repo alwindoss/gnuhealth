8f11b2cd998037b3a8ab5eb7eeda5d2bc0ad14ce
Luis Falcon <falcon@gnuhealth.org>
2021-08-10 17:38:25 +0100

    Use new direct access to backend classes

--- tryton/health/health.py
+++ tryton/health/health.py
@@ -2987,8 +2987,7 @@ class GnuHealthSequenceSetup(ModelSQL, ValueMixin):
 
     @classmethod
     def __register__(cls, module_name):
-        TableHandler = backend.get('TableHandler')
-        exist = TableHandler.table_exist(cls._table)
+        exist = backend.TableHandler.table_exist(cls._table)
 
         super(GnuHealthSequenceSetup, cls).__register__(module_name)
 
