73feeaf441ef74a726724250950fa9256d2cdb79
Luis Falcon <falcon@gnuhealth.org>
2021-12-21 00:15:45 +0000

    task #16043: Migration to GNU Health 4.0: Initial migration of health_nursing package

---
+++ tryton/health_nursing/exceptions.py
@@ -0,0 +1,9 @@
+# This file is part of GNU Health.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from trytond.exceptions import UserError, UserWarning
+from trytond.model.exceptions import ValidationError
+
+
+class NoAssociatedHealthProfessional(ValidationError):
+    pass
+
