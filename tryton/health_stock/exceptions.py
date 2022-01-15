9bc5bf7e9b74127cd3a1ef7328ce1088ef974493
Luis Falcon <falcon@gnuhealth.org>
2022-01-15 11:11:19 +0000

    task #16043: Migration to GNU Health 4.0: Migrate and cleanup health_stock

---
+++ tryton/health_stock/exceptions.py
@@ -0,0 +1,14 @@
+# This file is part of GNU Health.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from trytond.model.exceptions import ValidationError
+
+
+class ExpiredVaccine(ValidationError):
+    pass
+
+
+class StockMoveExists(ValidationError):
+    pass
+
+class NoPharmacy(ValidationError):
+    pass
