78f4f307382689e83176bcf5e2051a5f656b2d33
Luis Falcon <falcon@gnuhealth.org>
2022-01-15 07:16:33 +0000

    task #16043: Migration to GNU Health 4.0: Migrate and cleanup health_disability

--- tryton/health_disability/__init__.py
+++ tryton/health_disability/__init__.py
@@ -21,27 +21,28 @@
 #
 ##############################################################################
 
-from .health_disability import *
+from . import health_disability
 from trytond.pool import Pool
+
 
 def register():
     Pool.register(
-        GnuHealthPatient,
-        Product,
-        BodyFunctionCategory,
-        BodyFunction,
-        BodyStructureCategory,
-        BodyStructure,
-        ActivityAndParticipationCategory,
-        ActivityAndParticipation,
-        EnvironmentalFactorCategory,
-        EnvironmentalFactor,
-        PatientDisabilityAssessment,
-        PatientBodyFunctionAssessment,
-        PatientBodyStructureAssessment,
-        PatientActivityAndParticipationAsssessment,
-        PatientEnvironmentalFactorAssessment,
-        PatientAmputation,
-        PatientProthesis,
-        PatientData,
+        health_disability.GnuHealthPatient,
+        health_disability.Product,
+        health_disability.BodyFunctionCategory,
+        health_disability.BodyFunction,
+        health_disability.BodyStructureCategory,
+        health_disability.BodyStructure,
+        health_disability.ActivityAndParticipationCategory,
+        health_disability.ActivityAndParticipation,
+        health_disability.EnvironmentalFactorCategory,
+        health_disability.EnvironmentalFactor,
+        health_disability.PatientDisabilityAssessment,
+        health_disability.PatientBodyFunctionAssessment,
+        health_disability.PatientBodyStructureAssessment,
+        health_disability.PatientActivityAndParticipationAsssessment,
+        health_disability.PatientEnvironmentalFactorAssessment,
+        health_disability.PatientAmputation,
+        health_disability.PatientProthesis,
+        health_disability.PatientData,
         module='health_disability', type_='model')
