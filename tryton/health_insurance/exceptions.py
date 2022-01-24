71d2da6eeeb594eac2e57fa1b8cb49de8dd9cf88
Luis Falcon <falcon@gnuhealth.org>
2022-01-24 17:40:14 +0000

    health_insurance: migrate raise_user_error methods

--- tryton/health_insurance/exceptions.py
+++ tryton/health_insurance/exceptions.py
@@ -1,6 +1,6 @@
 # This file is part of GNU Health.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from trytond.exceptions import UserError, UserWarning
+from trytond.exceptions import UserError
 from trytond.model.exceptions import ValidationError
 
 
@@ -12,3 +12,13 @@ class DiscountWithoutElement(ValidationError):
     pass
 
 
+class ServiceInvoiced(UserError):
+    pass
+
+
+class NoInvoiceAddress(UserError):
+    pass
+
+
+class NoPaymentTerm(UserError):
+    pass
