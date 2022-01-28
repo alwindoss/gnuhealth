b9ce90de6e37f5ef3a51b71fa0f84f66818045fc
Luis Falcon <falcon@gnuhealth.org>
2022-01-28 16:53:13 +0000

    Fix bug #61848: Created Invoices menu taking too long to get invoices from database. Lint

--- tryton/health_services/invoice.py
+++ tryton/health_services/invoice.py
@@ -1,23 +1,43 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+##############################################################################
+#
+#    GNU Health: The Free Health and Hospital Information System
+#    Copyright (C) 2008-2022 Luis Falcon <lfalcon@gnusolidario.org>
+#    Copyright (C) 2011-2022 GNU Solidario <health@gnusolidario.org>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 from trytond.model import fields
 from trytond.pool import PoolMeta
-from trytond import backend
 
 __all__ = ['Invoice', 'InvoiceLine']
+
 
 class Invoice(metaclass=PoolMeta):
     __name__ = 'account.invoice'
 
     patient = fields.Function(
-            fields.Many2One('gnuhealth.patient', 'Patient',
-                help="Patient in the invoice"),
-                'get_patient')
+        fields.Many2One(
+            'gnuhealth.patient', 'Patient',
+            help="Patient in the invoice"),
+        'get_patient')
 
     health_service = fields.Function(
-            fields.Many2One('gnuhealth.health_service', 'Health Service',
-                help="The service entry"),
-                'get_health_service', searcher='search_health_service')
+        fields.Many2One(
+            'gnuhealth.health_service', 'Health Service',
+            help="The service entry"),
+        'get_health_service', searcher='search_health_service')
 
     def get_patient(self, name):
         try:
@@ -33,10 +53,11 @@ class Invoice(metaclass=PoolMeta):
 
     @classmethod
     def search_health_service(cls, name, clause):
-        return [('lines.origin.name.id',
-                    clause[1],
-                    clause[2],
-                    'gnuhealth.health_service.line')]
+        return [
+            ('lines.origin.name.id',
+             clause[1],
+             clause[2],
+             'gnuhealth.health_service.line')]
 
 
 class InvoiceLine(metaclass=PoolMeta):
