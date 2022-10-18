5483da90f74bfca86af8cc5a3c522bba69133a11
Luis Falcon <falcon@gnuhealth.org>
2022-10-18 12:23:32 +0100

    task #16232: Implement REUSE Initiative. Update dep5 report and documentation files on health package

--- tryton/health/report/__init__.py
+++ tryton/health/report/__init__.py
@@ -1,2 +1,14 @@
+# SPDX-FileCopyrightText: 2008-2022 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2022 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                           HEALTH package                              #
+#                __init__.py: Report declaration file                   #
+#########################################################################
 from . import health_report
 from . import immunization_status_report
