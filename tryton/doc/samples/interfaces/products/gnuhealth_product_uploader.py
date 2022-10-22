3b95e642809942462efe56023b7f232e40d96e1f
Luis Falcon <falcon@gnuhealth.org>
2022-10-22 12:32:29 +0100

    task #16232: Implement REUSE Initiative. Update headers for documentation sample files / scripts

--- tryton/doc/samples/interfaces/products/gnuhealth_product_uploader.py
+++ tryton/doc/samples/interfaces/products/gnuhealth_product_uploader.py
@@ -1,25 +1,17 @@
-#!/usr/bin/python3
-#  gnuhealth_product_uploader.py
-#  
-#  Copyright 2017 - 2020 Luis Falcon <falcon@gnuhealth.org>
-#  Copyright 2011-2022 GNU Solidario <health@gnusolidario.org>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
+#!/usr/bin/env python 
+# SPDX-FileCopyrightText: 2008-2022 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2022 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                       gnuhealth_product_uploader.py                   #
+#         Sample script to upload products from a CSV file              #
+#########################################################################
 
 # Requirements
 # Proteus version : 5.0.x 
