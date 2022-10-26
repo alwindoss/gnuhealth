5acd47908d38b5fcf3b90bacf05ed8b3a683d19f
Luis Falcon <falcon@gnuhealth.org>
2022-10-26 12:22:29 +0100

    task #16232: Implement REUSE Initiative. Update health_webdav3_server

--- tryton/health_webdav3_server/protocol.py
+++ tryton/health_webdav3_server/protocol.py
@@ -1,5 +1,9 @@
-# This file is part of GNU Health.  The COPYRIGHT file at the top level of
-# this repository contains the full copyright notices and license terms.
+# SPDX-FileCopyrightText: 2012-2017 Cédric Krier
+# SPDX-FileCopyrightText: 2017-2022 GNU Solidario <health@gnusolidario.org>
+# SPDX-FileCopyrightText: 2017-2022 Luis Falcon <falcon@gnuhealth.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 import socketserver
 import socket
 import http.server
