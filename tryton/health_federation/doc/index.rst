38007480807a03f7f917176adecbb95920827e06
Luis Falcon <falcon@gnuhealth.org>
2022-10-28 21:13:42 +0100

    task #16232: Implement REUSE Initiative. Update headers for health_federation

--- tryton/health_federation/doc/index.rst
+++ tryton/health_federation/doc/index.rst
@@ -1,4 +1,9 @@
+.. SPDX-FileCopyrightText: 2008-2022 Luis Falcón <falcon@gnuhealth.org>
+.. SPDX-FileCopyrightText: 2011-2022 GNU Solidario <health@gnusolidario.org>
+..
+.. SPDX-License-Identifier: CC-BY-SA-4.0
+
 GNU Health Federation Interface
 --------------------------------
 
-Module that will allow connecting and exchange information with the GNU Health Federation 
+Package to exchange information with the GNU Health Federation
--- tryton/health_federation/health_federation.py
+++ tryton/health_federation/health_federation.py
@@ -1,23 +1,16 @@
-##############################################################################
+# SPDX-FileCopyrightText: 2008-2022 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2022 GNU Solidario <health@gnusolidario.org>
 #
-#    GNU Health: The Free Health and Hospital Information System
-#    Copyright (C) 2008-2022 Luis Falcon <lfalcon@gnusolidario.org>
-#    Copyright (C) 2011-2022 GNU Solidario <health@gnusolidario.org>
-#
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                      HEALTH FEDERATION package                        #
+#                  health_federation.py: main module                    '#
+#########################################################################
 
 from trytond.model import ModelView, ModelSQL, ModelSingleton, fields, Unique
 from trytond.pool import Pool
