5ee4b9873687e73aa4470c158ee12063b294cc94
Luis Falcon <falcon@gnuhealth.org>
2022-10-28 18:08:21 +0100

    health_disability: PEP8 on setup.py

--- tryton/health_disability/setup.py
+++ tryton/health_disability/setup.py
@@ -19,8 +19,11 @@ import re
 import os
 import configparser
 
+
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname), encoding="UTF-8").read()
+    return open(os.path.join(os.path.dirname(__file__), fname),
+                encoding="UTF-8").read()
+
 
 config = configparser.ConfigParser()
 config.readfp(open('tryton.cfg'))
@@ -38,18 +41,24 @@ for dep in info.get('depends', []):
         requires.append('gnuhealth == %s' % (info.get('version')))
 
     elif dep.startswith('health_'):
-        health_package = dep.split('_',1)[1]
-        requires.append('gnuhealth_%s == %s' %
+        health_package = dep.split('_', 1)[1]
+        requires.append(
+            'gnuhealth_%s == %s' %
             (health_package, info.get('version')))
-    else: 
+    else:
         if not re.match(r'(ir|res|webdav)(\W|$)', dep):
-            requires.append('trytond_%s >= %s.%s, < %s.%s' %
+            requires.append(
+                'trytond_%s >= %s.%s, < %s.%s' %
                 (dep, major_version, minor_version, major_version,
                     minor_version + 1))
 
-setup(name='gnuhealth_disability',
+setup(
+    name='gnuhealth_disability',
     version=info.get('version', '0.0.1'),
-    description=info.get('description', 'GNU Health package for patient Functioning and disability, including WHO ICF'),
+    description=info.get(
+        'description',
+        'GNU Health package for patient Functioning and disability,'
+        ' including WHO ICF'),
     long_description=read('README'),
     author='GNU Solidario',
     author_email='health@gnusolidario.org',
@@ -62,10 +71,10 @@ setup(name='gnuhealth_disability',
         ],
 
     package_data={
-        'trytond.modules.health_disability': info.get('xml', []) \
-            + info.get('translation', []) \
-            + ['tryton.cfg', 'view/*.xml', 'doc/*.rst', 'locale/*.po',
-               'report/*.fodt', 'icons/*.svg'],
+        'trytond.modules.health_disability': info.get('xml', [])
+        + info.get('translation', [])
+        + ['tryton.cfg', 'view/*.xml', 'doc/*.rst', 'locale/*.po',
+           'report/*.fodt', 'icons/*.svg'],
         },
 
     classifiers=[
