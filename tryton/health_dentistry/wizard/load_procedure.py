f1f186058f79a5fb02de54b28087c6c9703806f0
Luis Falcon <falcon@gnuhealth.org>
2022-10-28 17:36:49 +0100

    task #16232: Implement REUSE Initiative. Update health_dentistry package

--- tryton/health_dentistry/wizard/load_procedure.py
+++ tryton/health_dentistry/wizard/load_procedure.py
@@ -1,3 +1,19 @@
+# SPDX-FileCopyrightText: 2020-2021 National University of Entre Rios (UNER)
+#                         School of Engineering
+#                         <saludpublica@ingenieria.uner.edu.ar>
+# SPDX-FileCopyrightText: 2020 Mario Puntin <mario@silix.com.ar>
+# SPDX-FileCopyrightText: 2020-2022 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2020-2022 GNU Solidario <health@gnusolidario.org>
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                         HEALTH DENTISTRY package                      #
+#         ad_procedure.py: Wizard to load/update procedures             #
+#########################################################################
 from trytond.model import ModelView, fields
 from trytond.wizard import Wizard, StateView, StateTransition, Button
 from trytond.pool import Pool
