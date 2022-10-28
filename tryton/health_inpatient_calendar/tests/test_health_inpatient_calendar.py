fddac8ae6c17ea21edfc21b8e90862e727064864
Luis Falcon <falcon@gnuhealth.org>
2022-10-28 14:55:58 +0100

    task #16232: Implement REUSE Initiative. Update health_inpatient_calendar

--- tryton/health_inpatient_calendar/tests/test_health_inpatient_calendar.py
+++ tryton/health_inpatient_calendar/tests/test_health_inpatient_calendar.py
@@ -1,3 +1,16 @@
+# SPDX-FileCopyrightText: 2008-2022 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2022 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                HEALTH INPATIENT CALENDAR package                      #
+#         test_health_inpatient_calendar.py main test module            #
+#########################################################################
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
