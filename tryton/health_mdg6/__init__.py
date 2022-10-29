2e4e826b5ab6969b20452a914bce69c0626bace6
Luis Falcon <falcon@gnuhealth.org>
2022-10-29 20:09:01 +0100

    task #16232: Implement REUSE Initiative. Update headers for health_mdg6. PEP8

--- tryton/health_mdg6/__init__.py
+++ tryton/health_mdg6/__init__.py
@@ -1,1 +1,12 @@
-from . import health_mdg6
+# SPDX-FileCopyrightText: 2008-2022 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2022 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                        HEALTH MDG6 package                            #
+#              __init__.py: Package declaration file                    #
+#########################################################################
