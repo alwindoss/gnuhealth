0264f4d228db71d8f574d34a005f467980af1889
Luis Falcon <falcon@gnuhealth.org>
2022-10-29 16:54:37 +0100

    task #16232: Implement REUSE Initiative. Update headers for health_lifestyle package. PEP8

--- tryton/health_lifestyle/__init__.py
+++ tryton/health_lifestyle/__init__.py
@@ -1,36 +1,26 @@
-# -*- coding: utf-8 -*-
-##############################################################################
+# SPDX-FileCopyrightText: 2008-2022 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2022 GNU Solidario <health@gnusolidario.org>
 #
-#    GNU Health: The Free Health and Hospital Information System
-#    Copyright (C) 2008-2022 Luis Falcon <lfalcon@gnusolidario.org>
-#    Copyright (C) 2011-2022 GNU Solidario <health@gnusolidario.org>
-#
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# SPDX-License-Identifier: GPL-3.0-or-later
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                      HEALTH LIFESTYLE package                         #
+#              __init__.py: Package declaration file                    #
+#########################################################################
 
 from trytond.pool import Pool
-from .health_lifestyle import *
+from . import health_lifestyle
 
 
 def register():
     Pool.register(
-        VegetarianTypes,
-        DietBelief,
-        DrugsRecreational,
-        PatientRecreationalDrugs,
-        PatientCAGE,
-        MedicalPatient,
+        health_lifestyle.VegetarianTypes,
+        health_lifestyle.DietBelief,
+        health_lifestyle.DrugsRecreational,
+        health_lifestyle.PatientRecreationalDrugs,
+        health_lifestyle.PatientCAGE,
+        health_lifestyle.MedicalPatient,
         module='health_lifestyle', type_='model')
