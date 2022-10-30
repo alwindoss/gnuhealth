760da6c5719c98faf25b11db7fc8733d97575140
Luis Falcon <falcon@gnuhealth.org>
2022-10-30 15:13:00 +0000

    task #16232: Implement REUSE Initiative. Update headers for health_socioeconomics

--- tryton/health_socioeconomics/tests/__init__.py
+++ tryton/health_socioeconomics/tests/__init__.py
@@ -1,1 +1,6 @@
+# SPDX-FileCopyrightText: 2008-2022 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2022 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from .test_health_socioeconomics import suite
