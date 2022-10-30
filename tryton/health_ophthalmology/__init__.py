22327a95051ee7d8d778f7ee7983281040c7ebe2
Luis Falcon <falcon@gnuhealth.org>
2022-10-30 12:27:39 +0000

    task #16232: Implement REUSE Initiative. Update headers for health_ophthalmology

--- tryton/health_ophthalmology/__init__.py
+++ tryton/health_ophthalmology/__init__.py
@@ -1,8 +1,22 @@
+# SPDX-FileCopyrightText: 2008-2022 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2022 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                       HEALTH OPHTHALMOGLOY package                    #
+#                  __init__.py: Package declaration file                #
+#########################################################################
+
 from trytond.pool import Pool
 from . import health_ophthalmology
+
 
 def register():
     Pool.register(
         health_ophthalmology.OphthalmologyEvaluation,
-        health_ophthalmology.OphthalmologyFindings,    
+        health_ophthalmology.OphthalmologyFindings,
         module='health_ophthalmology', type_='model')
