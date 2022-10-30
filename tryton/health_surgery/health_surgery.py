cf7af22df6c2499156d54152163ab15bdafa2db4
Luis Falcon <falcon@gnuhealth.org>
2022-10-30 15:33:25 +0000

    task #16232: Implement REUSE Initiative. Update headers for health_surgery

--- tryton/health_surgery/health_surgery.py
+++ tryton/health_surgery/health_surgery.py
@@ -1,26 +1,18 @@
-##############################################################################
-#
-#    GNU Health HMIS: The Free Health and Hospital Information System
-#    Copyright (C) 2008-2022 Luis Falcon <lfalcon@gnusolidario.org>
-#    Copyright (C) 2011-2022 GNU Solidario <health@gnusolidario.org>
-#
-#    The GNU Health HMIS component is part of the GNU Health project
-#    www.gnuhealth.org
+#!/usr/bin/env python
 
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# SPDX-FileCopyrightText: 2008-2022 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2022 GNU Solidario <health@gnusolidario.org>
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                      HEALTH SURGERY package                           #
+#                 health_surgery.py: Main module                        #
+#########################################################################
 import pytz
 from dateutil.relativedelta import relativedelta
 from trytond.model import ModelView, ModelSQL, fields, Unique
